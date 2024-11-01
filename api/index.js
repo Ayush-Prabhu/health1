3e61d1f48aab2cc2c474440cf4bd88a7871e0109
AYUSH PRABHU <ayushprabhu20@gmail.com>
2024-11-01 18:01:40 +0530

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -11,7 +11,11 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://healthmit.vercel.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 // Logging Middleware
 app.use((req, res, next) => {
