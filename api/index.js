66e3cb342ea1f90b1c554d8781bebe8a520197ca
AYUSH PRABHU <ayushprabhu20@gmail.com>
2024-11-01 13:59:35 +0000

    update cors

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: true,
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
