8953ee291a8b3a85995fa6d96aae6cd02c0cca49
AYUSH PRABHU <ayushprabhu20@gmail.com>
2024-11-01 13:53:24 +0000

    reset cors

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'https://health1-fe.vercel.app',
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
